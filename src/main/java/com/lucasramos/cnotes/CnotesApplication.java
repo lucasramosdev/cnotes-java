21c221e174cf581bf5a653b69920083090459098
Lucas Ramos <contato@lucasramos.dev.br>
2026-02-17 22:54:45 -0300

    chore: First Commit

---
+++ src/main/java/com/lucasramos/cnotes/CnotesApplication.java
@@ -0,0 +1,13 @@
+package com.lucasramos.cnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasramos/cnotes/CnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasramos.cnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

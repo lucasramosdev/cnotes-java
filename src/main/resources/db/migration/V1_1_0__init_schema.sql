cc07c7804dc9e1f10d061fe40e9fb477fbddaf64
Lucas Ramos <contato@lucasramos.dev.br>
2026-02-21 00:53:04 -0300

    chore: Creating init schema

---
+++ src/main/resources/db/migration/V1_1_0__init_schema.sql
@@ -0,0 +1,51 @@
+BEGIN;
+
+CREATE TABLE folders (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    parent_id INTEGER,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_folders
+        FOREIGN KEY (parent_id)
+        REFERENCES folders(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    folder_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    summary TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_folders_notes
+        FOREIGN KEY (folder_id)
+        REFERENCES folders(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE notes_tags (
+    note_id INTERGER NOT NULL,
+    value VARCHAR(25) NOT NULL,
+    PRIMARY KEY (note_id, value),
+    CONSTRAINT fk_notes_notes_tags
+        FOREIGN KEY (note_id)
+        REFERENCES notes(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    keyword VARCHAR(255) NOT NULL,
+    details TEXT NOT NULL,
+    note_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_notes_topics
+        FOREIGN KEY (note_id)
+           REFERENCES notes(id)
+           ON DELETE CASCADE
+);
+
+COMMIT;
